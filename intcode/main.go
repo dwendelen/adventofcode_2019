6a56b97d186b095e3ed30f1828ac30237b5bee02
Daan Wendelen <daanwendelen@gmail.com>
2019-12-13 22:08:03 +0100

    Disassembled program for day13

--- intcode/main.go
+++ intcode/main.go
@@ -3,14 +3,13 @@ package main
 import (
 	"../util"
 	"fmt"
-	"log"
 	"strconv"
 )
 
 func main() {
-	program := load("day11/input.txt")
-	//program[6] = program[6] + 5
-	printProg(program)
+	program := load("day13/input.txt")
+
+	printProg(program, []int{393}, []int{379, 639})
 }
 
 func load(file string) []int {
@@ -28,68 +27,80 @@ func load(file string) []int {
 	return values
 }
 
-func printProg(program []int) {
+func printProg(program []int, knownCodeStart []int, knownDataStart []int) {
 	pos := 0
+	isCode := true
 
 	for pos < len(program) {
 		fmt.Printf("% 4d ", pos)
 		fmt.Printf("%05d ", program[pos])
 
-		mode3, mode2, mode1, code := parse(program[pos])
-
-		switch code {
-		case 99: // halt
-			fmt.Printf("HLT")
-		case 1: // add
-			fmt.Printf("ADD ")
-			printArg(program[pos+1], mode1)
-			printArg(program[pos+2], mode2)
-			printArg(program[pos+3], mode3)
-			pos += 3
-		case 2: // multiply
-			fmt.Printf("MUL ")
-			printArg(program[pos+1], mode1)
-			printArg(program[pos+2], mode2)
-			printArg(program[pos+3], mode3)
-			pos += 3
-		case 3: // input
-			fmt.Printf("INP ")
-			printArg(program[pos+1], mode1)
-			pos += 1
-		case 4: // output
-			fmt.Printf("OUT ")
-			printArg(program[pos+1], mode1)
-			pos += 1
-		case 5: // jump-if-true
-			fmt.Printf("JPT ")
-			printArg(program[pos+1], mode1)
-			printArg(program[pos+2], mode2)
-			pos += 2
-		case 6: // jump-if-false
-			fmt.Printf("JPF ")
-			printArg(program[pos+1], mode1)
-			printArg(program[pos+2], mode2)
-			pos += 2
-		case 7: // less than
-			fmt.Printf("LES ")
-			printArg(program[pos+1], mode1)
-			printArg(program[pos+2], mode2)
-			printArg(program[pos+3], mode3)
-			pos += 3
-		case 8: // equals
-			fmt.Printf("EQL ")
-			printArg(program[pos+1], mode1)
-			printArg(program[pos+2], mode2)
-			printArg(program[pos+3], mode3)
-			pos += 3
-		case 9: // equals
-			fmt.Printf("REL ")
-			printArg(program[pos+1], mode1)
-			pos += 1
-		default:
+		mode3, mode2, mode1, code, ok := parse(program[pos])
+		if ok && isCode {
+			switch code {
+			case 99: // halt
+				fmt.Printf("HLT")
+			case 1: // add
+				fmt.Printf("ADD ")
+				printArg(program[pos+1], mode1)
+				printArg(program[pos+2], mode2)
+				printArg(program[pos+3], mode3)
+				pos += 3
+			case 2: // multiply
+				fmt.Printf("MUL ")
+				printArg(program[pos+1], mode1)
+				printArg(program[pos+2], mode2)
+				printArg(program[pos+3], mode3)
+				pos += 3
+			case 3: // input
+				fmt.Printf("INP ")
+				printArg(program[pos+1], mode1)
+				pos += 1
+			case 4: // output
+				fmt.Printf("OUT ")
+				printArg(program[pos+1], mode1)
+				pos += 1
+			case 5: // jump-if-true
+				fmt.Printf("JPT ")
+				printArg(program[pos+1], mode1)
+				printArg(program[pos+2], mode2)
+				pos += 2
+			case 6: // jump-if-false
+				fmt.Printf("JPF ")
+				printArg(program[pos+1], mode1)
+				printArg(program[pos+2], mode2)
+				pos += 2
+			case 7: // less than
+				fmt.Printf("LES ")
+				printArg(program[pos+1], mode1)
+				printArg(program[pos+2], mode2)
+				printArg(program[pos+3], mode3)
+				pos += 3
+			case 8: // equals
+				fmt.Printf("EQL ")
+				printArg(program[pos+1], mode1)
+				printArg(program[pos+2], mode2)
+				printArg(program[pos+3], mode3)
+				pos += 3
+			case 9: // equals
+				fmt.Printf("REL ")
+				printArg(program[pos+1], mode1)
+				pos += 1
+			default:
+			}
 		}
 		fmt.Printf("\n")
 		pos++
+		if len(knownCodeStart) > 0 && pos >= knownCodeStart[0] {
+			pos = knownCodeStart[0]
+			knownCodeStart = knownCodeStart[1:]
+			isCode = true
+		}
+		if len(knownDataStart) > 0 && pos >= knownDataStart[0] {
+			pos = knownDataStart[0]
+			knownDataStart = knownDataStart[1:]
+			isCode = false
+		}
 	}
 }
 func printArg(val int, mode int) {
@@ -106,10 +117,13 @@ func printArg(val int, mode int) {
 	fmt.Printf("%12s ", str)
 }
 
-func parse(instruction int) (mode3, mode2, mode1, code int) {
+func parse(instruction int) (mode3, mode2, mode1, code int, ok bool) {
 	if instruction >= 100000 {
-		log.Fatal("Illegal instruction " + strconv.Itoa(instruction))
+		ok = false
+		return
 	}
+	ok = true
+
 	code = instruction % 100
 	rest := instruction / 100
 	mode1 = rest % 10
